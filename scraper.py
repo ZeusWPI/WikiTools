14fe3cdbf7078a714c0fb42f5037f75b76fe84e0
Titouan Vervack <tivervac@gmail.com>
2014-08-10 13:52:57 +0200

    PEP8 guidelines

--- scraper.py
+++ scraper.py
@@ -5,17 +5,21 @@ from os import fsync, path, makedirs
 from datetime import date
 from re import M, I, findall, search, compile
 
-''' This class parses the index page which contains hyperlinks to the wiki pages'''
+
+# This class parses the index page which contains hyperlinks to the wiki pages
 class IndexPageParser(HTMLParser):
     def __init__(self, titles):
         super().__init__()
         self.titles = titles
 
-    ''' Find the titles of the wiki pages'''
+    # Find the titles of the wiki pages
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
-            if len(attrs) == 2 and attrs[0][0] == 'href' and attrs[1][0] == 'title':
+            if len(attrs) == 2
+            and attrs[0][0] == 'href'
+            and attrs[1][0] == 'title':
                 self.titles.append(attrs[1][1])
+
 
 class ImageLocater(HTMLParser):
     def __init__(self, imagelinks):
@@ -23,21 +27,22 @@ class ImageLocater(HTMLParser):
         self.imagelinks = imagelinks
         self.found = False
         self.data = ''
-        ''' The regex to look for external image links in usercreated source code'''
-        self.pattern = compile(r'\b((?:https?|ftp).*\.(?:png|jpe?g|gif|bmp))$', M | I)
+        # The regex to look for external image links in usercreated source code
+        self.pattern = compile(r'\b((?:https?|ftp).*\.(?:png|jpe?g|gif|bmp))$',
+                               M | I)
 
-    ''' Look for the textarea in the html page'''
+    # Look for the textarea in the html page
     def handle_starttag(self, tag, attrs):
         if tag == 'textarea':
             self.found = True
 
-    ''' Now save the data within it'''
+    # Now save the data within it
     def handle_data(self, data):
         if self.found:
             self.data += (data)
 
     def handle_endtag(self, tag):
-        ''' This is executed for each tag so check if it's th right tag'''
+        # This is executed for each tag so check if it's th right tag
         if tag == 'textarea':
             images = findall(self.pattern, self.data)
             if not images:
@@ -49,13 +54,14 @@ class ImageLocater(HTMLParser):
             self.found = False
 
 INDEX_PAGE = 'http://zeus.ugent.be/wiki/Speciaal:AllePaginas'
-SOURCE_CODE_LOCATION = 'http://zeus.ugent.be/wiki/index.php?title=%s&action=edit'
-IMAGE_PATH = 'images/'
+SOURCE_LOCATION = 'http://zeus.ugent.be/wiki/index.php?title=%s&action=edit'
+IMAGE_PATH = 'assets/'
 KEYWORD = '/wiki/Speciaal:AllePaginas'
 ENCODING = 'iso-8859_15'
 
+
 def get_pages():
-    ''' Download the index page containing hyperlinks to the other wiki pages'''
+    # Download the index page containing hyperlinks to the other wiki pages
     print('Fetching source...')
     try:
         html = urlopen(INDEX_PAGE).read().decode(ENCODING)
@@ -67,34 +73,40 @@ def get_pages():
         exit(2)
     print('Source has been fetched!')
 
-    ''' Narrow down the HTML'''
+    # Narrow down the HTML
     html = str(html).split(KEYWORD)
     titles = []
 
-    ''' Now find the titles in the narrowed down HTML'''
+    # Now find the titles in the narrowed down HTML
     parser = IndexPageParser(titles)
     parser.feed(html[1])
-    ''' Never forget to close :)'''
+    # Never forget to close
     parser.close()
 
     create_directory()
     get_images(titles)
 
+
 def create_directory():
     if not path.exists(IMAGE_PATH):
         makedirs(IMAGE_PATH)
 
+
 def get_images(titles):
     h = HTMLParser()
     current_title = 0
-    log_file = open(date.today().strftime('%Y-%m-%d %I-%M-%S%p') + '.log', 'wb')
+    log_file = open(date.today()
+                    .strftime('%Y-%m-%d %I-%M-%S%p') + '.log', 'wb')
     titles_length = len(titles)
     while current_title < titles_length:
-        print('Fetching images from %s... (%s/%s)' % (titles[current_title], current_title + 1, titles_length))
-        ''' Escape the title so we can create a valid link'''
-        title = titles[current_title].replace('\'','%27').replace(' ', '%20')
+        print('Fetching images from %s... (%s/%s)' %
+              (titles[current_title], current_title + 1, titles_length))
+        # Escape the title so we can create a valid link
+        title = titles[current_title].replace('\'', '%27').replace(' ', '%20')
         try:
-            page = urlopen(SOURCE_CODE_LOCATION % title).read().decode(ENCODING)
+            page = urlopen(SOURCE_LOCATION % title)
+            .read()
+            .decode(ENCODING)
         except:
             print("\tServer's being lazy, retrying...")
             continue
@@ -102,8 +114,9 @@ def get_images(titles):
             print('\tFailed to get %s\'s images!' % titles[current_title])
             current_title += 1
             continue
-        ''' Ignore redirects'''
-        if search('#DOORVERWIJZING', page, I | M) != None or search('#REDIRECT.*', page, I | M) != None:
+        # Ignore redirects
+        if search('#DOORVERWIJZING', page, I | M) is not None or
+        search('#REDIRECT.*', page, I | M) is not None:
             print('\tSkipping redirecting page %s' % titles[current_title])
             current_title += 1
             continue
@@ -115,19 +128,20 @@ def get_images(titles):
         parser.close()
 
         if imagelinks:
-            ''' Log the title of the page'''
+            # Log the title of the page
             log_file.write(bytes('%s\n' % title, ENCODING))
             for image_url in imagelinks:
-                ''' Log the image url'''
+                # Log the image url
                 log_file.write(bytes('\t%s' % image_url, ENCODING))
                 if not save_image(image_url):
                     log_file.write(bytes(' DEAD', ENCODING))
                 log_file.write(bytes('\n', ENCODING))
-            ''' Actually print something'''
+            # Actually print something
             log_file.flush()
             fsync(log_file.fileno())
         current_title += 1
     log_file.close()
+
 
 def save_image(image_url):
     if not path.exists(IMAGE_PATH + image_url.split('/')[-1]):
